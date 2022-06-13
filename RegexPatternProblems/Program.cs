a61340d8af6fdc8a91f436888fdf13bc34e02fd5
Shreema KN <shreemakn1013@gmail.com>
2022-06-13 18:54:26 +0530

    Add project files.

---
+++ RegexPatternProblems/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegexPatternProblems;
+
+class Program
+{
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine("User Registration Validating using regular expression!");
+        RegexPattern regexPattern = new RegexPattern();
+        regexPattern.Validating();
+
+    }
+}
+
